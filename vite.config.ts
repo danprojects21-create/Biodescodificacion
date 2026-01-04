df2c52a439e8583b8a6c58f408f107df8ecc00f4
danprojects21-create <danprojects21@gmail.com>
2026-01-04 01:27:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ESTA LÍNEA ES LA QUE ARREGLA LA PANTALLA EN BLANCO
   define: {
-    // Asegura que process.env esté disponible para la SDK de Gemini
     'process.env': process.env
   },
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    outDir: 'dist'
   }
 });
