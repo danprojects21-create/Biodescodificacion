e33660c4f85978e21cfaf150da439686ffd0c70f
danprojects21-create <danprojects21@gmail.com>
2026-01-04 23:39:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Vercel inyecta las variables de entorno, este mapeo asegura que estén en el bundle
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    'process.env': process.env
-  },
-  build: {
-    target: 'esnext',
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-  },
+  server: {
+    port: 3000
+  }
 });
